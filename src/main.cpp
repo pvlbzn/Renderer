3fffbe002fba07c7e1dfad7407f912d545e3a711
pvlbzn <pvlbzn@gmail.com>
2016-01-09 18:10:17 +0300

    Optimization

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,7 @@ void wrong_line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     bool steep = false;
+
     /* If the line is steep, we transpose the image. */
     if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
         std::swap(x0, y0);
@@ -28,16 +29,27 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
         std::swap(y0, y1);
     }
 
-    for (int x = x0; x <= x1; x++) {
-        float t = (x - x0) / (float)(x1 - x0);
-        int y = y0 * (1. - t) + y1 * t;
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+    int y = y0;
 
+    float derror = std::abs(dy / float(dx));
+    float  error = 0;
+
+    for (int x = x0; x <= x1; x++) {
         if (steep) {
             image.set(y, x, color); // if transposed, de-transpose
         } else {
             image.set(x, y, color);
         }
+        error += derror;
+
+        if (error > .5) {
+            y += (y1 > y0 ? 1 : -1);
+            error -= 1;
+        }
     }
+
 }
 
 int main() {
